238a51379ba548533b01a7f016321fa80da3de37
14Sandee <sandee.vit14@gmail.com>
2025-02-13 12:55:02 +0530

    fix: added tailwind config for vs code tailwind intellisense

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        './*.html', // Matches any HTML files in the root directory
+        './**/*.html' // Matches any HTML files in subdirectories
+      ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
